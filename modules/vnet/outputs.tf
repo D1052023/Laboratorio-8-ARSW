01a45fe5a98fc815a0b68177204474daec4f719a
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-03-22 12:28:48 -0500

    fix pipeline

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,3 +1,2 @@
-output "vmss_name" {
-  value = azurerm_linux_virtual_machine_scale_set.vmss.name
-}
+output "vnet_name" { value = azurerm_virtual_network.vnet.name }
+output "subnet_web_id" { value = azurerm_subnet.web.id }
