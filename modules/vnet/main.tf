0833b2e28d7fd84064a3acc9439a78c4a2ed621a
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-03-22 13:03:01 -0500

    reto 1

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -19,3 +19,32 @@ resource "azurerm_subnet" "mgmt" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.2.0/24"]
 }
+# Subred obligatoria para Bastion
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet" # Nombre estricto
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.10.0/26"] # Rango libre en tu espacio 10.10.x.x
+}
+
+# IP Pública para el servicio Bastion
+resource "azurerm_public_ip" "bastion_ip" {
+  name                = "${var.prefix}-bastion-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# El servicio de Bastion en sí
+resource "azurerm_bastion_host" "bastion" {
+  name                = "${var.prefix}-bastion"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion_ip.id
+  }
+}
