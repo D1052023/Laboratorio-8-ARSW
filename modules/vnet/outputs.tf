b41c82e82da2694745e1c4e9f1ce09d075091cda
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2026-03-22 09:51:01 -0500

    reto 1

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,2 +1,3 @@
-output "vnet_name" { value = azurerm_virtual_network.vnet.name }
-output "subnet_web_id" { value = azurerm_subnet.web.id }
+output "vmss_name" {
+  value = azurerm_linux_virtual_machine_scale_set.vmss.name
+}
